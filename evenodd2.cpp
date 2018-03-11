28863991cd85dd95fe95f09f9efedb646309da5a
Mohit Patil <35272471+mohit-patil@users.noreply.github.com>
2018-03-12 00:35:53 +0530

    Add files via upload

---
+++ evenodd2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int no;
+	cout<<"Enter Number";
+	cin>>no;
+	if((no/2)*2==no)
+		cout<<"Number is Even";
+	else
+		cout<<"Number is odd";
+}
---
+++ evenodd3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int no;
+	cout<<"Enter Number";
+	cin>>no;
+	if((no-1)/2==no/2)
+		cout<<"Number is Odd";
+	else
+		cout<<"Number is Even";
+}
