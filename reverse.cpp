1b9609c1b2e3e552e29a3ae9886806435440db71
Mohit Patil <35272471+mohit-patil@users.noreply.github.com>
2018-03-12 04:22:38 +0530

    Prog to reverse a number

---
+++ reverse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,rev=0,rem;
+	cout<<"Enter Number";	
+	cin>>num;
+	while(num!=0)
+	{
+		rem=num%10;
+		num=num/10;
+		rev=rev*10+rem;	
+	}
+	cout<<"Reverse is "<<rev;
+}
